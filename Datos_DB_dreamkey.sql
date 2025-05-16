4b312150f6bb4f58106e7118fcad6342e74c65ed
afr <arifragoso666@yahoo.com.mx>
2025-05-16 17:03:16 -0600

    datos para la tabla afiliados

--- Datos_DB_dreamkey.sql
+++ Datos_DB_dreamkey.sql
@@ -17,3 +17,35 @@ INSERT INTO DB_DreamKey.membresias (idMembresias, nombreMembresia, precioMembres
 (4, 'Paquete 4', 1800,'Estándar','un url 4', 'Incluye 10 servicios platino (Estándar) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.'),
 (5, 'Paquete 5', 1800,'Premium','un url 5', 'Incluye 5 servicios oro (Premium) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.'),
 (6, 'Paquete 6', 3500,'Premium','un url 6', 'Incluye 5 servicios oro (Premium) y puedes utilizarlas en las empresas afiliadas en el programa, consulta en la página de afiliados que empresas están disponibles con este nivel.');
+
+-- Datos para la tabla Afiliados 
+INSERT INTO DB_DreamKey.afiliados (
+    idAfiliados, nombreAfiliados, imgAfiliados, descripcionAfiliados,
+    ratingAfiliados, detallesAfiliados,
+    serviciosAfiliados1, serviciosAfiliados2, serviciosAfiliados3,
+    imgServiciosAfiliados1, imgServiciosAfiliados2, imgServiciosAfiliados3, Membresias_idMembresias
+) VALUES
+(1, 'Atelier Ross Melgar', '../../assets/img-afiliados/Atelier.jpg', 'Spa & salón de belleza', 4.5, 
+ 'Es un spa y salón de belleza que ofrece una experiencia única de relajación y cuidado personal. Con un equipo altamente capacitado brindando y cuidando siempre la calidad de sus servicios. Ofrecen una amplia gama de tratamientos faciales y corporales, así como servicios de peluquería y estética. Su ambiente acogedor y profesional garantiza una experiencia inolvidable para cada cliente.',
+ 'SPA', 'Corte', 'Masaje',
+ '../../assets/img-servicios/spa.jpg', '../../assets/img-servicios/corte.jpg', '../../assets/img-servicios/masaje.jpg', null),
+
+(2, 'Belleza Eterna', '../../assets/img-afiliados/Eterna.jpg', 'Spa', 3.1,
+ 'Es un spa que se especializa en tratamientos de belleza y bienestar. Ofrecen servicios con una calidad excepcional y un enfoque personalizado para cada cliente. Además de contar con productos de alta gamma y personal altamente capacitado para consentirte.',
+ 'Barbería', 'Corte', 'Facial',
+ '../../assets/img-servicios/barberia.jpg', '../../assets/img-servicios/corte.jpg','../../assets/img-servicios/faciales.jpg', null),
+
+(3, 'Luz y Brillo', '../../assets/img-afiliados/Luz.jpg', 'Barbería', 5,
+ 'Barbería especializada en cortes de cabello y afeitados. Ofrecen una experiencia única de relajación y servicio personalizado. Cuentan con certificaciones y un equipo que buscara siempre la calidad en sus servicios, con un estilo fresco y moderno.',
+ 'SPA', 'Corte', 'Masaje',
+ '../../assets/img-servicios/spa.jpg', '../../assets/img-servicios/corte.jpg', '../../assets/img-servicios/masaje.jpg', null),
+
+(4, 'Diva Glam', '../../assets/img-afiliados/Diva.jpg', 'Estudio de uñas', 4.8,
+ 'Es un estudio de uñas que diseña y crea uñas acrílicas, gelish y decoradas. Ofrecen una amplia gama de servicios de manicura y pedicura, con un enfoque en la calidad y la atención al detalle. Su equipo de profesionales está altamente capacitado para brindar una experiencia única y personalizada a cada cliente.',
+ 'Manicure', 'Pedicure', 'Gelish',
+ '../../assets/img-servicios/manicure.jpg', '../../assets/img-servicios/pedicure.jpg', '../../assets/img-servicios/gelish.jpg', null),
+
+(5, 'Elegance Studio', '../../assets/img-afiliados/Elegance.jpg', 'Centro de Belleza', 3.7,
+ 'Centro de belleza que ofrece una amplia gama de servicios para toda la familia, con un ambiente acogedor y profesional Elegance Studio busca brindar una experiencia para incluir a toda la familia y que puedan disfrutar y consentirse en familia, dando momentos inolvidables.',
+ 'Corte', 'Masaje', 'Peinados',
+ '../../assets/img-servicios/corte.jpg', '../../assets/img-servicios/masaje.jpg', '../../assets/img-servicios/peinados.jpg', null);
