6bfe2971121c8d958bee3eb618bc47704ac258f3
FredyA13 <fredy.alonzo2001@hotmail.com>
2025-05-20 12:41:52 -0600

    Se sube proyecto Springboot dreamkey

---
+++ DreamKeyAPI/src/main/java/org/generation/DreamKeyAPI/DreamKeyApiApplication.java
@@ -0,0 +1,13 @@
+package org.generation.DreamKeyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamKeyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamKeyApiApplication.class, args);
+	}
+
+}
