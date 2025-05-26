5d90caf95eb676ec2c289b972e1a038e5a3d42db
Adan Olea <adan.olea4580@alumnos.udg.mx>
2025-05-26 00:16:35 -0600

    Se arreglaron pequeños errores del comentarios java

--- DreamKeyAPI/src/main/java/org/generation/DreamKeyAPI/model/Comentarios.java
+++ DreamKeyAPI/src/main/java/org/generation/DreamKeyAPI/model/Comentarios.java
@@ -1,18 +1,30 @@
 package org.generation.DreamKeyAPI.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table; 
+
+@Entity
+@Table(name="Comentario")
 public class Comentarios {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
 	private String comentario;
-	private static Long total=Long.valueOf(0);
+	//private static Long total=Long.valueOf(0);
 	public Comentarios(String comentario) {
 		super();
 		this.comentario = comentario;
-		Comentarios.total++;
-		this.id=Comentarios.total;
+		//Comentarios.total++;
+		//this.id=Comentarios.total;
 	}
 	public Comentarios() {
-		Comentarios.total++;
-		this.id=Comentarios.total;
+		//Comentarios.total++;
+		//this.id=Comentarios.total;
 	}
 	public String getComentarios() {
 		return comentario;
@@ -20,9 +32,9 @@ public class Comentarios {
 	public void setComentarios(String comentario) {
 		this.comentario = comentario;
 	}
-	public static Long getTotal() {
+	/*public static Long getTotal() {
 		return total;
-	}
+	} */
 	public Long getId() {
 		return id;
 	}
