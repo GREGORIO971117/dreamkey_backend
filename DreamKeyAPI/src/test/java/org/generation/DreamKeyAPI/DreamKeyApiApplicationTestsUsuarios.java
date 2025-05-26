0db416b1871621cfec47559174b5ab359e73f716
LUU <luuosoriio@gmail.com>
2025-05-26 13:43:30 -0600

    test usuario

--- DreamKeyAPI/src/test/java/org/generation/DreamKeyAPI/DreamKeyApiApplicationTestsUsuarios.java
+++ DreamKeyAPI/src/test/java/org/generation/DreamKeyAPI/DreamKeyApiApplicationTestsUsuarios.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DreamKeyApiApplicationTests {
+class DreamKeyApiApplicationTestsUsuarios {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		
 	}
 
 }
