8967f5e2a590cab7f79a1856673d4cc98d3c3746
Hector Valdez <hectorvaldez1907@gmail.com>
2025-05-25 20:16:16 -0600

    Se sube suscripcion acyualizada

--- DreamKeyAPI/src/main/java/org/generation/DreamKeyAPI/model/Suscripcion.java
+++ DreamKeyAPI/src/main/java/org/generation/DreamKeyAPI/model/Suscripcion.java
@@ -2,68 +2,101 @@ package org.generation.DreamKeyAPI.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-
 @Entity
-@Table(name = "suscripcion")
+@Table(name = "Suscripcion")
 public class Suscripcion {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "idSuscripcion", unique = true, nullable = false)
-	private Long idSuscripcion;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idSuscripcion", unique = true, nullable = false)
+    private Long idSuscripcion;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(nullable = false)
+    @Column(name = "fechaSuscripcion", nullable = false)
     private LocalDate fechaSuscripcion = LocalDate.now();
 
     @JsonFormat(pattern = "yyyy-MM-dd")
-    @Column(nullable = false)
+    @Column(name = "fechaPagoSuscripcion", nullable = false)
     private LocalDate fechaPagoSuscripcion = LocalDate.now().plusDays(30);
-    
-    @Column(name = "activaSuscripcion", nullable = false)
+
+    @Column(name = "suscripcionActiva", nullable = false)
     private Boolean suscripcionActiva;
 
+    @ManyToOne
+    @JoinColumn(name = "idMembresias_fk", nullable = false)
+    private Membresias membresia;
 
-	public Suscripcion(
-			LocalDate fechaSuscripcion, 
-			LocalDate fechaPagoSuscripcion, 
-			Boolean suscripcionActiva) {
-		
-		this.fechaSuscripcion = fechaSuscripcion;
-		this.fechaPagoSuscripcion = fechaPagoSuscripcion;
-		this.suscripcionActiva = suscripcionActiva;
-	}
-	
-	public Suscripcion() {
-	}
+    @ManyToOne
+    @JoinColumn(name = "Usuario_idUsuario", nullable = false)
+    private Usuarios usuario;
 
-	public LocalDate getFechaSuscripcion() {
-		return fechaSuscripcion;
-	}
-	public void setFechaSuscripcion(LocalDate fechaSuscripcion) {
-		this.fechaSuscripcion = fechaSuscripcion;
-	}
-	public LocalDate getFechaPagoSuscripcion() {
-		return fechaPagoSuscripcion;
-	}
-	public void setFechaPagoSuscripcion(LocalDate fechaPagoSuscripcion) {
-		this.fechaPagoSuscripcion = fechaPagoSuscripcion;
-	}
-	public Boolean getSuscripcionActiva() {
-		return suscripcionActiva;
-	}
+    // Constructores
+    public Suscripcion() {
+    }
+
+    public Suscripcion(
+            LocalDate fechaSuscripcion,
+            LocalDate fechaPagoSuscripcion,
+            Boolean suscripcionActiva,
+            Membresias membresia,
+            Usuarios usuario) {
+        this.fechaSuscripcion = fechaSuscripcion;
+        this.fechaPagoSuscripcion = fechaPagoSuscripcion;
+        this.suscripcionActiva = suscripcionActiva;
+        this.membresia = membresia;
+        this.usuario = usuario;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return idSuscripcion;
+    }
+
+    public LocalDate getFechaSuscripcion() {
+        return fechaSuscripcion;
+    }
+
+    public void setFechaSuscripcion(LocalDate fechaSuscripcion) {
+        this.fechaSuscripcion = fechaSuscripcion;
+    }
+
+    public LocalDate getFechaPagoSuscripcion() {
+        return fechaPagoSuscripcion;
+    }
+
+    public void setFechaPagoSuscripcion(LocalDate fechaPagoSuscripcion) {
+        this.fechaPagoSuscripcion = fechaPagoSuscripcion;
+    }
+
+    public Boolean getSuscripcionActiva() {
+        return suscripcionActiva;
+    }
+
     public void setSuscripcionActiva(Boolean suscripcionActiva) {
         this.suscripcionActiva = suscripcionActiva;
     }
-    
+
+    public Membresias getMembresia() {
+        return membresia;
+    }
+
+    public void setMembresia(Membresias membresia) {
+        this.membresia = membresia;
+    }
+
+    public Usuarios getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuarios usuario) {
+        this.usuario = usuario;
+    }
+
+    // Método para renovar la suscripción
     public void renovarSuscripcion() {
         if (LocalDate.now().isAfter(fechaPagoSuscripcion) || LocalDate.now().isEqual(fechaPagoSuscripcion)) {
             this.fechaSuscripcion = LocalDate.now();
@@ -72,16 +105,12 @@ public class Suscripcion {
         }
     }
 
-	public Long getId() {
-		return idSuscripcion;
-	}
-
-	@Override
-	public String toString() {
-		return "Suscripcion [id=" + idSuscripcion + ", fechaSuscripcion=" + fechaSuscripcion + ", fechaPagoSuscripcion="
-				+ fechaPagoSuscripcion + ", suscripcionActiva=" + suscripcionActiva + "]";
-	}
-	
-	
+    @Override
+    public String toString() {
+        return "Suscripcion [id=" + idSuscripcion + ", fechaSuscripcion=" + fechaSuscripcion
+                + ", fechaPagoSuscripcion=" + fechaPagoSuscripcion + ", suscripcionActiva=" + suscripcionActiva
+                + ", membresia=" + membresia + ", usuario=" + usuario + "]";
+    }
 }
+
 
