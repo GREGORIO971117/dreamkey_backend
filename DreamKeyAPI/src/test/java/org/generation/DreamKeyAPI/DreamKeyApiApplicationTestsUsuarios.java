34a6c9e30f2e73b07fac6fe3a0fa8671964a1d03
LUU <luuosoriio@gmail.com>
2025-05-26 15:21:05 -0600

    pruebas

--- DreamKeyAPI/src/test/java/org/generation/DreamKeyAPI/DreamKeyApiApplicationTestsUsuarios.java
+++ DreamKeyAPI/src/test/java/org/generation/DreamKeyAPI/DreamKeyApiApplicationTestsUsuarios.java
@@ -3,6 +3,13 @@ package org.generation.DreamKeyAPI;
 import org.junit.jupiter.api.Test; 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+
+
 @SpringBootTest
 class DreamKeyApiApplicationTestsUsuarios {
 
